ae3d83562fed0948c269439e6ecaa7735d9ea633
OguzhanKadakal <76901135+OguzhanKadakal@users.noreply.github.com>
2025-05-21 21:14:35 +0300

    Add ESLint and Prettier configuration, update webpack setup, and create initial HTML template

--- webpack.common.js
+++ webpack.common.js
@@ -1,18 +1,34 @@
+// config/webpack.common.js
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: {
-    app: './src/index.js',
+  entry: './src/index.js',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, '../dist'),
+    clean: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Production',
+      template: './src/index.html',
+      title: 'Webpack Template',
     }),
   ],
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: ['html-loader'],
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'asset/resource',
+      }
+    ],
   },
 };
